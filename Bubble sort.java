d5adfb9b91b627d2189ac6ad139fa3b074824ab8
JAI SINGH <67551769+jaisrm98@users.noreply.github.com>
2020-07-11 17:55:34 +0530

    Add files via upload

---
+++ Bubble sort.java
@@ -0,0 +1,34 @@
+package sorting;
+import java.util.Scanner;
+
+public class buuble_sort {
+	   public static void main(String []args) {
+		   int n;
+		   Scanner sc=new Scanner(System.in);
+		   n=sc.nextInt();
+		   sc.nextLine();
+		  
+		int ar[]=new int[n];
+		for(int i=0;i<n;i++)
+			ar[i]=sc.nextInt();
+		int temp=0;
+		 
+		System.out.println("numbers in sorted order:");
+		for (int i = 0; i <n; i++) {
+	   	   for (int j = i + 1; j < n; j++) {
+			
+			if (ar[i]<ar[j])
+			{
+				temp = ar[i];
+				ar[i] = ar[j];
+				ar[j] = temp;
+			}
+	   	   }
+		   
+	   	   
+		   System.out.print(ar[i]+" ");
+			
+	   	   
+		}
+	   }
+	}
