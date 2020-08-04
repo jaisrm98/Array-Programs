f2b1569c4e1139d93aa7b68ca3127b200a3e8191
JAI SINGH <jai.srm98@gmail.com>
2020-08-04 23:03:49 +0530

    Add files via upload

---
+++ Shift_zero_left.java
@@ -0,0 +1,28 @@
+package coding;
+
+public class Shift_zero_left {
+
+	public static void main(String[] args) {
+		int arr[]= {1,0,1,0,1,1};
+		int c=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==0)
+			{
+				c++;
+			}
+		}
+		for(int i=0;i<c;i++)
+		{
+			arr[i]=0;
+		}
+		for(int i=c;i<arr.length;i++)
+			arr[i]=1;
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+
+}
