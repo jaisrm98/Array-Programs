f1c8c24d24ca4797aeddfcfba93c890a096897fa
JAI SINGH <jai.srm98@gmail.com>
2020-08-04 22:47:08 +0530

    Add files via upload

---
+++ Seperate_Zero_One.java
@@ -0,0 +1,38 @@
+package coding;
+
+public class Seperate_Zero_One {
+
+	public static void main(String[] args) {
+		int arr[]= {0,1,0,1,1};
+		int zero=0;
+		int ones=0;
+		int d[]=new int[arr.length];
+		int c[]=new int[arr.length];
+		for(int i=0;i<arr.length;i++)
+		{
+			
+			if(arr[i]==0)
+			{
+				zero++;
+			}
+			else
+				ones++;
+		}
+			for(int j=0;j<zero;j++)
+				 d[j]=0;
+		for(int j=0;j<ones;j++)
+			c[j]=1;
+	
+		for(int i=0;i<zero;i++)
+		{
+			System.out.print(d[i]);
+		}
+		System.out.println();
+		for(int i=0;i<ones;i++)
+		{
+			System.out.print(c[i]);
+		}
+
+	}
+
+}
