c1223d490db8c4d27262ab695ce461860258ae7c
JAI SINGH <jai.srm98@gmail.com>
2020-08-05 11:38:34 +0530

    Add files via upload

---
+++ Rotate_Array.java
@@ -0,0 +1,27 @@
+package coding;
+import java.util.*;
+public class Rotate_Array {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5};
+		//String str="a b c d e";        //for string rotation
+		 // String [] arr = str.split(" ");
+		  
+		  int n=3;
+		  int j; 
+		  int first;
+		  for(int i=0;i<n;i++)
+		  {
+			  
+		  first=arr[0];
+		  for(j=0;j<arr.length-1;j++)
+		  {
+			  arr[j]=arr[j+1];
+		  }
+		  arr[j]=first;
+		  }
+		  System.out.println("left rotation");
+		  for(int i=0;i<arr.length;i++)
+			  System.out.print(arr[i]+" ");
+	}
+}
