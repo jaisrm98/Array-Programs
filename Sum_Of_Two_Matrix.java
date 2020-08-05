af315b3f767724507a017dd45bf3edc0e50b37fb
JAI SINGH <jai.srm98@gmail.com>
2020-08-06 01:50:08 +0530

    Add files via upload

---
+++ Sum_Of_Two_Matrix.java
@@ -0,0 +1,49 @@
+package coding;
+
+import java.util.Scanner;
+
+public class Sum_Of_Two_Matrix {
+
+	public static void main(String[] args) {
+		int m,n;
+		
+		Scanner sc=new Scanner(System.in);
+		m=sc.nextInt();
+		n=sc.nextInt();
+		int arr[][]=new int[m][n];
+		int arr1[][]=new int[m][n];
+		int sum[][]=new int[m][n];
+		System.out.println("First matrix");
+		for(int i=0;i<m;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		System.out.println("Second matrix");
+		for(int i=0;i<m;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				arr1[i][j]=sc.nextInt();
+			}
+		}
+		 
+		for(int i=0;i<m;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				sum[i][j]=arr[i][j]+arr1[i][j];
+			}
+	    }
+		for(int i=0;i<m;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print(sum[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
