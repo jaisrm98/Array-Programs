3b8476cfe402a674b2608d8c01648bd4cf5fbe49
JAI SINGH <jai.srm98@gmail.com>
2020-08-05 20:54:05 +0530

    Add files via upload

---
+++ Diagonal_Sum_Of_Matrix.java
@@ -0,0 +1,24 @@
+package coding;
+import java.util.*;
+public class Diagonal_Sum_Of_Matrix {
+
+	public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int n,m;
+n=sc.nextInt();
+int arr[][]=new int[n][n];
+int sum=0;
+for(int i=0;i<n;i++)
+{
+	for(int j=0;j<n;j++)
+	{
+		arr[i][j]=sc.nextInt();
+		if(i==j || (i+j)==n-1)
+			sum+=arr[i][j];
+	}
+}
+System.out.println(sum);
+
+	}
+
+}
