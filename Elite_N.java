f949ee2c2ebc302aaea70d6a5e85fd3ae3663102
JAI SINGH <jai.srm98@gmail.com>
2020-08-06 21:48:32 +0530

    Add files via upload

---
+++ Elite_N.java
@@ -0,0 +1,34 @@
+package coding;
+import java.util.*;
+public class Elite_N {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int d=1;
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		int k=sc.nextInt();
+		int x=k;
+		for(int i=0;i<n;)
+		{
+			if(k>arr[i])
+			{
+				k=k-arr[i];
+				i++;
+			}
+			else
+			{
+				k=x;
+				d++;
+			}
+				
+		}
+		System.out.println("day= "+d);
+	}
+
+}
