60e42bbbfdf718a131d6cac753c6b2301167527c
JAI SINGH <jai.srm98@gmail.com>
2020-08-07 23:02:21 +0530

    Add files via upload

---
+++ Decimal_to_Binary.java
@@ -0,0 +1,23 @@
+package coding;
+import java.util.*;
+public class Decimal_to_Binary {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int i=0;
+		int arr[]=new int[65];
+		while(n!=0)
+		{
+			arr[i]=n%2;
+			n=n/2;
+			i++;
+		}
+		for( i=i-1;i>=0;i--)
+		{
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
