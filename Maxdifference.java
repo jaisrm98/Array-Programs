b68c9d71218bb1ca91663f5842e151dc71eb5a57
JAI SINGH <jai.srm98@gmail.com>
2020-08-12 19:16:08 +0530

    Add files via upload

---
+++ Maxdifference.java
@@ -0,0 +1,30 @@
+package coding;
+import java.util.*;
+public class Maxdifference {
+	static int difference(int arr[],int n)
+	{
+		int max=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=n-1;j>i;j--)
+			{
+				if(arr[j]>arr[i] && (j-i)>max)
+					max=(j-i);
+			}
+		}
+		return max;
+	}
+	public static void main(String[] args) {
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+			arr[i]=sc.nextInt();
+		
+		int output=difference(arr, n);
+		System.out.println("max distance "+output);
+
+	}
+
+}
