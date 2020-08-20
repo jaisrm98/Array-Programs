4d06961652c3dfe56616072b9edac8857d0c26df
JAI SINGH <jai.srm98@gmail.com>
2020-08-20 14:35:00 +0530

    Update dublicate_element_in_array.java

--- dublicate_element_in_array.java
+++ dublicate_element_in_array.java
@@ -1,3 +1,4 @@
+
 package sorting;
 import java.util.Scanner;
 public class dublicate_element_in_array {
@@ -5,6 +6,7 @@ public class dublicate_element_in_array {
 	public static void main(String[] args) {
 		
 		int n;
+		int c=0;
 		Scanner sc=new Scanner(System.in);
 		n=sc.nextInt();
 		int arr[]=new int[n];
@@ -15,10 +17,22 @@ public class dublicate_element_in_array {
 		{
 			for(int j=i+1;j<n;j++)
 			{
-				if(arr[i]==arr[j])
+				if(arr[i]==arr[j]) {
+					 
 					System.out.print(arr[i]+" ");
+					c++;
+				}
+				
+					
 			}
 		}
+		System.out.println("total dublicate= " +c);
+		System.out.println("total dublicate elements= " +c*2);
 	}
 
 }
+
+	Input:	5
+		1 1 2 2 3
+	Output:	1 2 total dublicate= 2
+		total dublicate elements= 4         //1 1 2 2
