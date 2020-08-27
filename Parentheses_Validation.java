29ba3c76a1f98ffd3817b1eb1ad6ec9ae1dc3c04
JAI SINGH <jai.srm98@gmail.com>
2020-08-27 19:23:13 +0530

    Add files via upload

---
+++ Parentheses_Validation.java
@@ -0,0 +1,30 @@
+package coding;
+import java.util.*;
+public class Parentheses_Validation {
+
+		static boolean valid(String s)
+		{
+			if(s.length()%2!=0)
+				return false;
+			Stack<Character>stk=new Stack<Character>();
+			for(char c:s.toCharArray())
+				if(c=='('||c=='['||c=='{')
+					stk.push(c);
+				else if(c==')' && !stk.isEmpty() && stk.peek()=='(')
+					stk.pop();
+				else if(c==']' && !stk.isEmpty() && stk.peek()=='[')
+					stk.pop();
+				else if(c=='}' && !stk.isEmpty() && stk.peek()=='{')
+					stk.pop();
+			return stk.isEmpty();
+			
+		}
+	public static void main(String[] args) {
+		String s;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextLine();
+		boolean test=valid(s);
+		System.out.println(test);
+	}
+
+}
