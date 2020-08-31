a93e66ffa574e1fe2a307f485e46b64a39ad601a
JAI SINGH <jai.srm98@gmail.com>
2020-08-31 18:10:35 +0530

    Add files via upload

---
+++ Continuous_Subarray_gives_largestsum.java
@@ -0,0 +1,32 @@
+package coding;
+
+public class Continuous_Subarray_gives_largestsum {
+
+	public static void main(String[] args) {
+		int nums[]= {-2,1,-3,4,-1,2,1,-5,4};
+		int sum=0;int result=nums[0];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i==0)
+                sum=nums[i];
+            
+            else
+            {  
+                if(nums[i]>=(sum+nums[i]))
+                {
+                    sum=nums[i];
+                }
+               
+                else {
+                	sum=sum+nums[i];
+                }
+               
+                result=Math.max(result, sum);
+                
+            }
+            
+            }
+        System.out.println(result);
+	}
+
+}
