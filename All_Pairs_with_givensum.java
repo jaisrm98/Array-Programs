733630757b189f0f07c3756223a79ed96c8be541
JAI SINGH <jai.srm98@gmail.com>
2020-09-03 20:12:54 +0530

    Add files via upload

---
+++ All_Pairs_with_givensum.java
@@ -0,0 +1,25 @@
+package coding;
+
+public class All_Pairs_with_givensum {
+
+
+
+		public static void main(String[] args) {
+			int target=9;
+			int A[]={6,7,2,3};
+		
+			for(int i = 0; i < A.length - 1; i++)
+	        {
+	            for(int j = i + 1; j < A.length; j++)
+	            {
+	                if(A[i] + A[j] == target)
+	                {
+	                 System.out.println("("+A[i]+", "+A[j]+")");
+	                }
+	            }
+		}
+
+
+	}
+	}
+}
