eac1136880f530b67cfd3be5ddeb688ed6230e3b
JAI SINGH <jai.srm98@gmail.com>
2020-09-07 17:48:48 +0530

    Add files via upload

---
+++ Max_Frequency_InArray.java
@@ -0,0 +1,41 @@
+package coding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Max_Frequency_InArray {
+
+	public static void main(String[] args) {
+		int arr[]= {1,1,2,3,3,3};
+		HashMap<Integer, Integer>count=new HashMap<Integer,Integer>();
+		ArrayList<Integer>list=new ArrayList<Integer>();
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			if(!list.contains(arr[i]))
+			{
+				list.add(arr[i]);
+				count.put(arr[i], 1);
+			}
+			else
+			{
+				count.put(arr[i],count.get(arr[i])+1);
+			}
+		}
+		for (Map.Entry entry : count.entrySet())  
+            System.out.println("Key = " + entry.getKey() + 
+                             ", frequency = " + entry.getValue());
+		int max =0;
+		int key=0;
+		for(int k:count.keySet())
+		{
+			if(count.get(k)>max) {
+				max=count.get(k);
+						key=k;
+		}
+	}
+		System.out.println(key+" is having max value "+ max);
+	}
+
+}
