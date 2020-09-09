ae16b2d2879a3eb7426de9965e873a3110b7324e
JAI SINGH <jai.srm98@gmail.com>
2020-09-10 01:27:13 +0530

    Add files via upload

---
+++ Find_Maxfrequency_of_continuous_repeated_element.java
@@ -0,0 +1,25 @@
+package coding;
+
+public class Find_Maxfrequency_of_continuous_repeated_element {
+
+	public static void main(String[] args) {
+		int arr[]= {1,0,1,1,1,0,1,1};
+		int c=0;
+		int result=0;
+		for(int i:arr)
+		{
+			if(i==1)
+			{
+				c++;
+				result=Math.max(result, c);
+			}
+			else
+			{
+				c=0;
+			}
+		}
+		System.out.println(result);
+
+	}
+
+}
