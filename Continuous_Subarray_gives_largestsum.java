7a6741ca67205990620f9396a1a9462b95362fa4
JAI SINGH <jai.srm98@gmail.com>
2020-10-13 02:35:56 +0530

    Update Continuous_Subarray_gives_largestsum.java

--- Continuous_Subarray_gives_largestsum.java
+++ Continuous_Subarray_gives_largestsum.java
@@ -4,7 +4,7 @@ public class Continuous_Subarray_gives_largestsum {
 
 	public static void main(String[] args) {
 		int nums[]= {-2,1,-3,4,-1,2,1,-5,4};
-		int sum=0;int result=nums[0];
+		int sum=0;int result=0;
         for(int i=0;i<nums.length;i++)
         {
             if(i==0)
