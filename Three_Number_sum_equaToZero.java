c36258792a662a57d8f95bb1245d309a67e36224
JAI SINGH <jai.srm98@gmail.com>
2020-11-26 19:24:36 +0530

    Add files via upload

---
+++ Three_Number_sum_equaToZero.java
@@ -0,0 +1,28 @@
+package coding;
+
+public class Three_Number_sum_equaToZero {
+
+	public static void main(String[] args) {
+		int arr[]= {3,-1,-7,-4,-5,9,-4};
+		int n=arr.length;
+		int sum=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				for(int k=j+1;k<n;k++)
+				{
+					
+					if(arr[i]+arr[j]+arr[k]==0)
+						System.out.println(arr[i]+" "+arr[j]+" "+arr[k]);
+				}
+				
+				
+			}
+			
+			
+		}
+
+	}
+
+}
