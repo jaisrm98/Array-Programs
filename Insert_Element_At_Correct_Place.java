3bbaf17cf78d58e48fe3154fcce2ecf87d7917fd
JAI SINGH <jai.srm98@gmail.com>
2021-01-17 17:17:36 +0530

    Add files via upload

---
+++ Insert_Element_At_Correct_Place.java
@@ -0,0 +1,30 @@
+package coding;
+
+public class Insert_Element_At_Correct_Place {
+
+	static public int nums(int [] nums,int target)
+	{
+		 int i;
+	        for( i=0;i<nums.length;i++)
+	        {
+	            if(target==nums[i])
+	                return i;
+	            else if(nums[i]>target)
+	                return i;
+	        }
+	        return i;
+
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+		int nums[]= {1,3,5,6};
+				int target=2;
+				System.out.println(nums(nums,target));
+				
+		
+
+	}
+
+}
