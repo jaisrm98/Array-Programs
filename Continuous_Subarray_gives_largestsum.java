c81001c33fde090015fc65b23ee08f4b03223dc8
JAI SINGH <jai.srm98@gmail.com>
2021-06-19 16:07:00 +0530

    Update Continuous_Subarray_gives_largestsum.java

--- Continuous_Subarray_gives_largestsum.java
+++ Continuous_Subarray_gives_largestsum.java
@@ -1,35 +1,24 @@
-package coding;
-
-public class Continuous_Subarray_gives_largestsum {
+package Practice;
+import java.util.*;
+public class Continuous_Sub_Array_LargestSum {
 
 	public static void main(String[] args) {
-		int nums[]= {-2,1,-3,4,-1,2,1,-5,4};
-		int sum=0;int result=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(i==0)
-                sum=nums[i];
-            
-            else
-            {  
-                if(nums[i]>=(sum+nums[i]))
-                {
-                    sum=nums[i];
-                }
-               
-                else {
-                	sum=sum+nums[i];
-                }
-               
-                result=Math.max(result, sum);
-                
-            }
-            
-            }
-        System.out.println(result);
+		int arr[]= {-2,1,-3,4,-1,2,1,-5,4};
+		int sum=0,result=0;
+		for(int i:arr) {
+			if(i>sum+i)
+				sum=i;
+			else
+				sum=sum+i;
+			
+			result=Math.max(result,sum);
+		}
+		System.out.println(result);
+
 	}
 
 }
+
 
 input: -2,1,-3,4,-1,2,1,-5,4
 Output: 6               // 4,-1,2,1
