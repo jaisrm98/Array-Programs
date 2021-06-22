457db2a6fe5ec6ad8e103e9327be6b2d0ba0e232
JAI SINGH <jai.srm98@gmail.com>
2021-06-22 15:24:51 +0530

    Update Missing_Number.java

--- Missing_Number.java
+++ Missing_Number.java
@@ -1,37 +1,31 @@
-package coding;
-
-import java.util.*;
+package Practice;
 
 public class Missing_Number {
-
-	public static int missingNumber(int[] arr) {
-		int k = 0;
-		if (arr.length == 1) {
-			if (arr[0] == 1)
-				return 0;
+	static int missing(int arr[])
+	{
+		int k=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==i)
+				continue;
 			else
-				return 1;
-
+			{
+				k=i;
+				break;
+				
+			}
 		}
 		
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] != i) {
-				k = i;
-				break;
-			} else
-
-				k = arr.length;
-		}
 		return k;
 	}
-
+	
+	
 	public static void main(String[] args) {
-		int arr[] = {0,2};
-		Arrays.sort(arr);
-		int output = missingNumber(arr);
-		System.out.println(output);
+		int arr[]= {0,1,2,3,4,6,7};
+		System.out.println(missing(arr));
+
 	}
 
 }
 
-Output: 1
+Output: 5
