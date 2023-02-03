e5105dd37cc6e4d5e57264850a0e280cb92520d2
JAI SINGH <jai.srm98@gmail.com>
2023-02-03 17:54:54 +0530

    Update Decimal_to_Binary.java

--- Decimal_to_Binary.java
+++ Decimal_to_Binary.java
@@ -3,24 +3,21 @@ import java.util.*;
 public class Decimal_to_Binary {
 
 	public static void main(String[] args) {
-		int n;
-		Scanner sc=new Scanner(System.in);
-		n=sc.nextInt();
-		int i=0;
-		int arr[]=new int[65];
-		while(n!=0)
+		int n=6;
+		ArrayList<Integer>list=new ArrayList<Integer>();
+		
+		while(n>0)
 		{
-			arr[i]=n%2;
+			list.add(n%2);
 			n=n/2;
-			i++;
 		}
-		for( i=i-1;i>=0;i--)
-		{
-			System.out.print(arr[i]);
-		}
+		int size=list.size();
+		
+		for(int i=size-1;i>=0;i--)
+			System.out.print(list.get(i)+"");
 	}
 
 }
 
-Input: 2
-Output: 10
+Input: 6
+Output: 110
