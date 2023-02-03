7b73f2f182376a66ec56961c1035371d477369a5
JAI SINGH <jai.srm98@gmail.com>
2023-02-03 20:06:11 +0530

    Update Peek_Element.java

--- Peek_Element.java
+++ Peek_Element.java
@@ -1,23 +1,23 @@
-package coding;
+package Code;
 
-public class Peek_Element {
+public class peak_element {
 
 	public static void main(String[] args) {
-		int arr[]= {5, 10, 20, 15};
+		int arr[]= {20,1};
 		if(arr.length==1)
 			System.out.println("Peek element index is "+0);
-		if(arr[0]>arr[1])
+		if(arr.length==2 && arr[0]>arr[1])
 			System.out.println("Peek element index is "+0);
-		if(arr[arr.length-1]>arr[arr.length-2])
+		if(arr.length==2 && arr[arr.length-1]>arr[0])
 			System.out.println("Peek element index is " +(arr.length-1));
 		for(int i=1;i<arr.length-1;i++)
 		{
 			if(arr[i]>arr[i-1]&& arr[i]>arr[i+1])
 				System.out.println("Peek element index is "+i);
 		}
-
 	}
 
 }
+
 
 Output: Peek element index is 2
